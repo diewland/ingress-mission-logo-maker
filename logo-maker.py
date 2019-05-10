082fa8c03631d9dc21743e6c5a271531f77e57a0
diewland <diewland@gmail.com>
2019-05-10 11:30:58 +0700

    to python3

--- logo-maker.py
+++ logo-maker.py
@@ -1,4 +1,3 @@
-import numpy as np
 import argparse
 import cv2
 import os
@@ -9,17 +8,17 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 
 h, w          = image.shape[:2]  # get height, weight of image
-logo_w        = w / 6            # find logo width ( 6 logos in a row )
-total_row     = h / logo_w       # find row number
+logo_w        = int(w / 6)       # find logo width ( 6 logos in a row )
+total_row     = int(h / logo_w)  # find row number
 total_mission = 6 * total_row    # find number of missions
 
-print "\n##### INFO #####\n"
-print "img height\t:", h
-print "img width\t:", w
-print "logo width\t:", logo_w
-print "total row\t:", total_row
-print "total mission\t:", total_mission
-print "\n##### LOG #####\n"
+print("\n##### INFO #####\n")
+print("img height\t:", h)
+print("img width\t:", w)
+print("logo width\t:", logo_w)
+print("total row\t:", total_row)
+print("total mission\t:", total_mission)
+print("\n##### LOG #####\n")
 
 # create ./out folder
 newpath = r'./out' 
@@ -43,6 +42,6 @@ for r in range(0, total_row): # loop each row
 
         #print cur_fname,
         #print "%s-%s" % ( r, c ),
-        print "%s:%s,%s:%s\t" % ( from_y, to_y, from_x, to_x ),
-    print
-print
+        print("%s:%s,%s:%s\t" % ( from_y, to_y, from_x, to_x ), end=" ")
+    print()
+print()
